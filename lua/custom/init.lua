e419a640386a58c7ce42774aea15e200536d14f9
Tyler <tfriesen2@my.gcu.edu>
2026-03-21 22:34:55 -0700

    added an AI agent plugin as well as some QOL plugins to support

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,5 +1,12 @@
 -- Add my custom keymaps to open Neotree and shift between tabs quickly
+-- toggle neo tree (command + n)
 vim.keymap.set('n', '<C-n>', ':Neotree toggle reveal_force_cwd<cr>')
+-- tab navigation
 vim.keymap.set('n', '<tab>', ':tabn<cr>')
+-- tab backwards navigation
 vim.keymap.set('n', '<S-tab>', ':tabN<cr>')
+-- new tab
 vim.keymap.set('n', '<leader>T', ':tabnew<cr>', { noremap = true, silent = true })
+
+-- AI Chat Toggle
+vim.keymap.set({ 'n', 'v' }, '<leader>aa', '<cmd>CodeCompanionChat Toggle<cr>', { desc = 'AI Chat Toggle' })
--- lua/custom/plugins/custom_plugins.lua
+++ lua/custom/plugins/custom_plugins.lua
@@ -4,9 +4,7 @@ return {
     'iamcco/markdown-preview.nvim',
     cmd = { 'Markdownpreviewtoggle', 'Markdownpreview', 'Markdownpreviewstop' },
     build = 'cd app && yarn install',
-    init = function()
-      vim.g.mkdp_filetypes = { 'markdown' }
-    end,
+    init = function() vim.g.mkdp_filetypes = { 'markdown' } end,
     ft = { 'markdown' },
   },
 
@@ -26,6 +24,76 @@ return {
           mappings = {
             ['\\'] = 'close_window',
           },
+        },
+      },
+    },
+  },
+
+  {
+    'olimorris/codecompanion.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'hrsh7th/nvim-cmp',
+      'stevearc/dressing.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+    config = function()
+      require('codecompanion').setup {
+        strategies = {
+          chat = { adapter = 'ollama' },
+          inline = { adapter = 'ollama' },
+          agent = { adapter = 'ollama' },
+        },
+        opts = {
+          language = 'en',
+          send_code = true, -- Always send current file context
+          use_diagnostic_errors = true, -- Let AI see LSP diagnostics
+        },
+        display = {
+          chat = {
+            window = {
+              layout = 'vertical', -- Open as a sidebar on the right
+              width = 45, -- Set a clean sidebar width
+            },
+          },
+          diff = {
+            provider = 'mini_diff',
+          },
+        },
+        adapters = {
+          ollama = function()
+            return require('codecompanion.adapters').extend('ollama', {
+              schema = {
+                model = {
+                  default = 'qwen2.5-coder:32b',
+                },
+                num_ctx = {
+                  default = 16384,
+                },
+              },
+            })
+          end,
+        },
+      }
+    end,
+  },
+
+  -- this is for the markdown preview in the AI agent window
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown", "codecompanion" }
+  },
+
+  -- this allows us to paste images into the chat
+  {
+    "HakonHarnes/img-clip.nvim",
+    opts = {
+      filetypes = {
+        codecompanion = {
+        prompt_for_file_name = false,
+        template = "[Image]($FILE_PATH)",
+        use_absolute_path = true,
         },
       },
     },
